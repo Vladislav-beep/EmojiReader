5084cd48e109902a0259fe9dcb881e4540a70e47
VLADISLAV <SIZONOV.95@MAIL.RU>
2021-09-25 21:26:55 +0300

    setup cell

---
+++ EmojiReader/EmojiTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiReader
+//
+//  Created by Владислав Сизонов on 25.09.2021.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
